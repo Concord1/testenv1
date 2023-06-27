5925ee2ddddb2ba1d067e4618c5e457d9ea8686b
Harish Kanagal <78932555+Concord1@users.noreply.github.com>
2023-06-27 15:57:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
 
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <script>
-            const PORT = process.env.PORT;
-            console.log(PORT);
-        </script>
-    </head>
-    <body>
-        
-    </body>
-    </html>
-  `;
+  const html = 'main.html';
 
   res.end(html);
 });
