3240a6bc830e084ad0bfe2c70ef9ccaf3049fc75
Harish Kanagal <78932555+Concord1@users.noreply.github.com>
2023-06-27 21:10:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const http = require('http');
-const fs = require('fs');
-const ejs = require('ejs');
-
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
 
-  const templatePath = 'template.ejs';
-  const name = 'John'; // Replace with the desired value
-  const PORTO = process.env.PORT
-  fs.readFile(templatePath, 'utf8', (err, template) => {
-    if (err) {
-      console.error(err);
-      res.end('<h1>Error reading template file</h1>');
-    } else {
-      const renderedHtml = ejs.render(template, { PORTO });
-      res.end(renderedHtml);
-    }
-  });
-});
+  const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <script>
+            const PORT = process.env.PORT;
+            console.log(PORT);
+        </script>
+    </head>
+    <body>
+        
+    </body>
+    </html>
+  `;
+  const html = 'main.html';
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  res.end(html);
 });
