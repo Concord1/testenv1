ca51912d6bf28eda9bb2ded7485064406483be9d
Harish Kanagal <78932555+Concord1@users.noreply.github.com>
2023-06-27 15:59:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
 
-  const html = 'main.html';
+  const html = '/main.html';
 
   res.end(html);
 });
